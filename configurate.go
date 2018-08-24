d4c2bf4d1e9af62e7be370b0bf7b3232d2dbf6fc
Jaap Groeneveld <jgroeneveld@fastmail.com>
2018-08-24 08:28:52 +0200

    make nice

--- configurate.go
+++ configurate.go
@@ -10,6 +10,7 @@ import (
 	"strconv"
 )
 
+// LoadFile loads a configuration from file into target and uses the default loaders.
 func LoadFile(path string, target interface{}) error {
 	file, err := os.Open(path)
 	defer file.Close()
@@ -20,10 +21,12 @@ func LoadFile(path string, target interface{}) error {
 	return LoadAll(target, NewJSONLoader(file), NewEnvLoader(), NewDefaultsLoader(), NewRequiredLoader())
 }
 
+// Loader is an interface to define generic configuration loaders
 type Loader interface {
 	Load(target interface{}) error
 }
 
+// LoadAll runs all loaders and stops when an error occurs
 func LoadAll(target interface{}, loaders ...Loader) error {
 	for _, loader := range loaders {
 		if err := loader.Load(target); err != nil {
@@ -33,14 +36,17 @@ func LoadAll(target interface{}, loaders ...Loader) error {
 	return nil
 }
 
+// JSONLoader is a Loader for JSON
 type JSONLoader struct {
 	Reader io.Reader
 }
 
+// NewJSONLoader returns a JSONLoader
 func NewJSONLoader(reader io.Reader) *JSONLoader {
 	return &JSONLoader{Reader: reader}
 }
 
+// Load executes the loading
 func (l *JSONLoader) Load(target interface{}) error {
 	decoder := json.NewDecoder(l.Reader)
 	err := decoder.Decode(target)
@@ -50,13 +56,16 @@ func (l *JSONLoader) Load(target interface{}) error {
 	return nil
 }
 
+// DefaultsLoader is a loader that loads the defaults from the `default` tag, if there is not already a value
 type DefaultsLoader struct {
 }
 
+// NewDefaultsLoader returns a DefaultsLoader
 func NewDefaultsLoader() *DefaultsLoader {
 	return &DefaultsLoader{}
 }
 
+// Load executes the loading
 func (l *DefaultsLoader) Load(target interface{}) error {
 	targetValue := reflect.ValueOf(target).Elem()
 	targetType := targetValue.Type()
@@ -83,20 +92,23 @@ func (l *DefaultsLoader) Load(target interface{}) error {
 				f.SetInt(int64(converted))
 			}
 		default:
-			return errors.New(fmt.Sprintf("No default loader defined for type %s", field.Type))
+			return fmt.Errorf("No default loader defined for type %s", field.Type)
 		}
 	}
 
 	return nil
 }
 
+// EnvLoader loads from the env, specified by the env tag
 type EnvLoader struct {
 }
 
+// NewEnvLoader returns a EnvLoader
 func NewEnvLoader() *EnvLoader {
 	return &EnvLoader{}
 }
 
+// Load executes the loading
 func (l *EnvLoader) Load(target interface{}) error {
 	targetValue := reflect.ValueOf(target).Elem()
 	targetType := targetValue.Type()
@@ -124,20 +136,23 @@ func (l *EnvLoader) Load(target interface{}) error {
 			}
 			f.SetInt(int64(converted))
 		default:
-			return errors.New(fmt.Sprintf("No env loader defined for type %s", field.Type))
+			return fmt.Errorf("No env loader defined for type %s", field.Type)
 		}
 	}
 
 	return nil
 }
 
+// RequiredLoader checks all non-pointer values for presence and returns an error if not given.
 type RequiredLoader struct {
 }
 
+// NewRequiredLoader returns a RequiredLoader
 func NewRequiredLoader() *RequiredLoader {
 	return &RequiredLoader{}
 }
 
+// Load executes the loading
 func (l *RequiredLoader) Load(target interface{}) error {
 	targetValue := reflect.ValueOf(target).Elem()
 	targetType := targetValue.Type()
@@ -149,16 +164,16 @@ func (l *RequiredLoader) Load(target interface{}) error {
 		switch field.Type.Kind() {
 		case reflect.String:
 			if f.String() == "" {
-				return errors.New(fmt.Sprintf("Required Value %q missing", field.Name))
+				return fmt.Errorf("Required Value %q missing", field.Name)
 			}
 		case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
 			if f.Int() == 0 {
-				return errors.New(fmt.Sprintf("Required Value %q missing", field.Name))
+				return fmt.Errorf("Required Value %q missing", field.Name)
 			}
 		case reflect.Ptr:
 			// ignore, pointers can be optional
 		default:
-			return errors.New(fmt.Sprintf("No required loader defined for type %s", field.Type))
+			return fmt.Errorf("No required loader defined for type %s", field.Type)
 		}
 	}
 
